c1084b7b08540f2975593ae775b4957d6b39b14a
theaddamsC <vpnforru@gmail.com>
2019-12-10 21:48:23 +0800

    Simple Chatroom

--- socket-exercise/index.js
+++ socket-exercise/index.js
@@ -9,13 +9,30 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/views/index.html');
 })
 
+var count = 0;
+
 io.on('connection', function(socket){
     console.log('Hey~');
+    count++;
+    io.emit("online", count);
 
+
+    socket.on('greet', function(){
+        socket.emit('greet', count);
+    })
     socket.on('disconnect', function(){
         console.log('Bye~');
+        count = (count < 0)? 0: count-1;
+        io.emit("online", count);
     })
+
+    socket.on("send", function(msg){
+        console.log(msg);
+        io.emit('msg', msg);
+    });
 })
+
+
 
 server.listen(8888, function(){
     console.log('port:8000~');
